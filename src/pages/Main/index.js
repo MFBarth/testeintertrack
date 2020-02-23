84aaa79737db4b7be43194d6ba7e9f3ccc05dfea
Matheus Barth Francisco <MFBarth@localhost.localdomain>
2020-02-23 12:39:00 -0300

    Design Finalizado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 
-import { Titulo, Container, Form } from './styles';
+import {
+  Titulo,
+  Container,
+  Form,
+  CheckBox,
+  Field,
+  Label,
+  Button,
+} from './styles';
+
+const options = [
+  { value: 'react', label: 'React' },
+  { value: 'angular', label: 'Angular' },
+  { value: 'vue', label: 'Vue' },
+];
 
 function Main() {
+  const [name, setName] = useState('');
+  const [biography, setBiography] = useState('');
+  const [experience, setExperience] = useState();
+  const [lib, setLib] = useState();
+  const [startDate, setStartDate] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <>
       <Titulo>Pesquisa de Recrutamento</Titulo>
 
       <Container>
-        <h1>Nome</h1>
-        <Form>
-          <input type="text" placeholder="Digite seu Nome" />
-        </Form>
+        <Form onSubmit={handleSubmit}>
+          <Field>
+            <h2>Nome</h2>
+            <input required type="text" placeholder="Digite seu Nome" />
+          </Field>
 
-        <h1>Biografia</h1>
-        <Form>
-          <textarea rows="10" cols="40" placeholder="Digite sua Biografia" />
+          <Field>
+            <h2>Biografia</h2>
+            <textarea
+              required
+              rows="10"
+              cols="40"
+              placeholder="Digite sua Biografia"
+            />
+          </Field>
+
+          <Field>
+            <h2>Experiências Adicionais</h2>
+            <CheckBox>
+              <Label htmlFor="bdd">
+                <input id="bdd" name="bdd" type="checkbox" />
+                BDD
+              </Label>
+              <Label htmlFor="git">
+                <input id="git" name="git" type="checkbox" />
+                GIT
+              </Label>
+              <Label htmlFor="unittests">
+                <input id="unittests" name="unittests" type="checkbox" />
+                UNITTESTS
+              </Label>
+            </CheckBox>
+          </Field>
+          <Field>
+            <h2>JS Library of Choice</h2>
+            <Select placeholder="Selecione" options={options} />
+          </Field>
+          <Field>
+            <h2>Start Date</h2>
+            <input required type="date" />
+          </Field>
+          <Button type="submit">Enviar formulário</Button>
         </Form>
       </Container>
     </>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -9,13 +9,14 @@ export const Titulo = styled.h1`
 
 export const Container = styled.div`
   max-width: 600px;
-  border-radius: 4px;
+  border-radius: 8px;
   padding: 30px;
   margin: 80px auto;
 
-  h1 {
+  h2 {
     color: #fff;
-    font-size: 20px;
+    font-size: 22px;
+    margin-bottom: 10px;
   }
 `;
 
@@ -23,25 +24,68 @@ export const Form = styled.form`
   margin-top: 20px;
   margin: 0;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding: 10px 0;
   margin-bottom: 30px;
+  max-width: 500px;
+
+  input,
+  textarea {
+    /* flex: 1; */
+    width: 100%;
+    border: 1px solid #eee;
+    background: #efefef;
+    border-radius: 5px;
+    box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
+    padding: 10px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+
+  [type='date'] {
+    background: #fff
+      url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)
+      97% 50% no-repeat;
+  }
+  [type='date']::-webkit-inner-spin-button {
+    display: none;
+  }
+  [type='date']::-webkit-calendar-picker-indicator {
+    opacity: 0;
+  }
+`;
+
+export const Field = styled.div`
+  margin-top: 20px;
+`;
+
+export const CheckBox = styled.div`
+  display: flex;
 
   input {
-    flex: 1;
-    border: 1px solid #eee;
-    box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
-    padding: 10px 15px;
-    border-radius: 4px;
-    font-size: 16px;
+    cursor: pointer;
+    border-radius: 3px;
+    width: 25px;
+    height: 16px;
   }
+`;
 
-  textarea {
-    flex: 1;
-    border: 1px solid #eee;
-    box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
-    padding: 10px 15px;
-    border-radius: 4px;
-    font-size: 16px;
-  }
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 18px;
+  margin-right: 10px;
+  font-weight: bold;
+`;
+
+export const Button = styled.button`
+  background: #fff;
+  padding: 5px;
+  margin-top: 20px;
+  border: #fff;
+  border-radius: 5px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #7159c1;
 `;
